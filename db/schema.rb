b0110a1b9475e5daf7022916255a52b37320960e
Ethan <ethanjneyland@gmail.com>
2022-05-03 23:06:45 +1000

    post models and relations pt2 class

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_03_053519) do
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
-    t.string "publisher"
-    t.date "date_published"
+ActiveRecord::Schema[7.0].define(version: 2022_05_03_122804) do
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "publisher"
+    t.date "date_published"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "author_id", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  add_foreign_key "books", "authors"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,22 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Author.destroy_all
+
 # Book.destroy_all
 # Deletes all books entered through SQL in IRB
 # Now only contains the books that were hardcoded
 # Used for development, not for a live site
 
-Book.create(title: "Drop Bear", author: "Evelyn Araluen", publisher: "University of Queensland Press", date_published: "2021-03-03")
-Book.create(title: "Fiber Fueled", author: "Will Bulsiewicz", publisher: "Penguin", date_published: "2020-02-02")
-Book.create(title: "The Power of Now", author: "Eckhart Tolle", publisher: "Namaste Publishing", date_published: "2004-04-04")
-Book.create(title: "To Kill a Mockingbird", author: "Harper Lee", publisher: "Arrow Books", date_published: "2006-06-06")
-Book.create(title: "This Boy's Life", author: "Tobias Wolff", publisher: "Bloomsbury Publishing", date_published: "1999-09-09")
+ara = Author.create(first_name: "Evelyn", last_name: "Araluen")
+bul = Author.create(first_name: "Will", last_name: "Bulsiewicz")
+tol = Author.create(first_name: "Eckhart", last_name: "Tolle")
+lee = Author.create(first_name: "Harper", last_name: "Lee")
+wol = Author.create(first_name: "Tobias", last_name: "Wolff")
+
+
+ara.books.create(title: "Drop Bear", publisher: "University of Queensland Press", date_published: "2021-03-03")
+bul.books.create(title: "Fiber Fueled", publisher: "Penguin", date_published: "2020-02-02")
+tol.books.create(title: "The Power of Now", publisher: "Namaste Publishing", date_published: "2004-04-04")
+lee.books.create(title: "To Kill a Mockingbird", publisher: "Arrow Books", date_published: "2006-06-06")
+wol.books.create(title: "This Boy's Life", publisher: "Bloomsbury Publishing", date_published: "1999-09-09")
